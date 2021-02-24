fb2a0ae273d452417bfe2bdc62802e0d012eebf2
장성호 <tjdgh925@gmail.com>
2021-02-25 00:26:34 +0900

    #7 useRef와 배열 사용법

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import InputSample from './InpustSample';
+import React, {useRef, useState} from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
+// import InputSample from './InpustSample';
 // import Counter from './Counter';
 // import Hello from './Hello';
 // import Wrapper from './Wrapper';
@@ -8,9 +10,73 @@ import InputSample from './InpustSample';
 
 
 function App() {
-  return (
-    <InputSample />
-  );
-}
-
-export default App;
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  });
+  const {username, email} = inputs;
+  const onChange = e =>{
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  };
+  const [users, setUsers] =useState([   
+    {
+        id: 1,
+        username: 'Jang',
+        email: 'tjdgh92@gmail.com',
+        active: true
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com',
+        active: false
+    },
+    {
+        id: 3,
+        username: 'kim',
+        email: 'kim@example.com',
+        active: false
+    }
+    ]);
+    const nextId = useRef(4);
+    const onCreate = () =>{
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers(users.concat(user));
+      setInputs({
+        username:'',
+        email:''
+      });
+      nextId.current += 1;
+    };
+    const onRemove = id =>{
+      setUsers(users.filter(user => user.id !== id));
+    };
+    const onToggle = id =>{
+        setUsers(
+          users.map(user=>
+            user.id === id ? { ...user, active: !user.active} : user
+            )
+        );
+    };
+    return (
+      <>
+        <CreateUser
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+        <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      </>
+    );
+  }
+  
+  export default App;
--- src/InpustSample.js
+++ src/InpustSample.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample(){
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+
+    const nameInput = useRef();
+
     const {name, nickname} = inputs;
     
     const onChange = (e) => {
@@ -18,7 +21,8 @@ function InputSample(){
         setInputs({
             name:'',
             nickname:''
-        })
+        });
+        nameInput.current.focus();
     };
     return(
         <div>
@@ -27,13 +31,14 @@ function InputSample(){
             placeholder="이름"
             onChange={onChange}
             value={name}
+            ref={nameInput}
             />
             <input 
             name="nickname"
             placeholder="닉네임"
             onChange={onChange}
             value={nickname}/>
-            <button onClick={onChange}>초기화</button>
+            <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
                 {name} ({nickname})
